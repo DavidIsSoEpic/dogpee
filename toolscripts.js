d707cdfbf68eb3e71b631f0e36406040d3ba89e9
dogpee <157509341+DavidIsSoEpic@users.noreply.github.com>
2024-07-02 11:40:23 +0200

    Update toolscripts.js

--- toolscripts.js
+++ toolscripts.js
@@ -8,17 +8,26 @@ document.getElementById('conversion-form').addEventListener('submit', function(e
 
     const conversionFactors = {
         'meters_feet': 3.28084,
+        'feet_meters': 1 / 3.28084,
         'kilograms_pounds': 2.20462,
+        'pounds_kilograms': 1 / 2.20462,
         'liters_gallons': 0.264172,
-        'celsius_fahrenheit': value => value * 9/5 + 32,
-        // Add more conversions as needed
+        'gallons_liters': 1 / 0.264172,
+        'celsius_fahrenheit': value => value * 9 / 5 + 32,
+        'fahrenheit_celsius': value => (value - 32) * 5 / 9
     };
 
     if (fromUnit === 'celsius' && toUnit === 'fahrenheit') {
         result = conversionFactors['celsius_fahrenheit'](value);
+    } else if (fromUnit === 'fahrenheit' && toUnit === 'celsius') {
+        result = conversionFactors['fahrenheit_celsius'](value);
     } else {
         const conversionKey = `${fromUnit}_${toUnit}`;
-        result = value * conversionFactors[conversionKey];
+        if (conversionFactors[conversionKey]) {
+            result = value * conversionFactors[conversionKey];
+        } else {
+            result = 'Conversion not supported.';
+        }
     }
 
     document.getElementById('result').innerText = `Result: ${result}`;
